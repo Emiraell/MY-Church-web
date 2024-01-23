7530cb6282a389842b2d071030a517dfb7849550
emirael <emmzex19@gmail.com>
2024-01-23 22:43:58 +0100

    God of all impossibilty component

--- src/pages/goai/Goai.jsx
+++ src/pages/goai/Goai.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 import Header from "../../components/Header/Header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExchange, faMicrophone } from "@fortawesome/free-solid-svg-icons";
+import goai from "./assests/GOAI.mp4";
+import Footer from "../../components/footer/Footer";
 
 export default function Goai() {
   const [days, setDays] = useState(0);
@@ -23,26 +27,112 @@ export default function Goai() {
     }, 1000);
     return () => clearInterval(intervalId);
   }, []);
-  //console.log(currentDate, programDate, distance);
+  const [daetails, setDetails] = useState([
+    {
+      title: "GREAT SPEAKERS",
+      image: <FontAwesomeIcon icon={faMicrophone} className="px-2" />,
+      content:
+        "Yearly, God blesses us with ministrations from the Holy Spirit through his servants thereby annoucing Himself mightly",
+    },
+    {
+      title: "GLORIOUS ECOUNTER",
+      image: <FontAwesomeIcon icon={faExchange} className="px-2" />,
+      content:
+        "Ecounters with the realms of Heavens are made yearly at the cause of this program. The Lost are found and the aleady found are rekindled",
+    },
+    {
+      title: "MIRACLES",
+      image: <FontAwesomeIcon icon={faMicrophone} className="px-2" />,
+      content:
+        "Miracles are administered by Heavens and Holy Spirit at the course of this program. Are raised heads are been met with the power of Christ",
+    },
+    {
+      title: "GREAT COMMISION",
+      image: <FontAwesomeIcon icon={faExchange} className="px-2" />,
+      content:
+        "The Holy Spirit is also in the business of raising and commison people for the propagation of the gospel during the cause of this program",
+    },
+  ]);
   return (
     <div className="bg-gray-300 text-center text-textCol-primary">
       <Header page={"GOD OF ALL IMPOSSIBILITIES"} />
       <div>
-        <div className="flex justify-around text-xl py-5">
-          <p>
-            {days} <span className="block">Days</span>
-          </p>
-          <p>
-            {hours} <span className="block">Hours</span>
-          </p>
-          <p>
-            {minutes} <span className="block">Minutes</span>
-          </p>
-          <p>
-            {seconds}
-            <span className="block">Seconds</span>
-          </p>
+        <p className="mt-5 text-xl font-bold font-lato">
+          GOD OF ALL IMPOSSIBILITIES 2024
+        </p>
+        <div className="flex-col lg:flex, justify-around text-xl py-5 font-roboto tracking-wider">
+          <div className="flex justify-evenly">
+            <p className=" border-dashed py-7 px-8 font-bold border-8 rounded-full border-blue-500">
+              <span>{days < 10 ? `0${days}` : days}</span>{" "}
+              <span className="block">Days</span>
+            </p>
+            <p className=" border-dashed p-7 font-bold border-8 rounded-full border-yellow-500">
+              <span>{hours < 10 ? `0${hours}` : hours}</span>{" "}
+              <span className="block">Hours</span>
+            </p>
+          </div>
+          <div className="flex justify-evenly my-10">
+            <p className=" border-dashed py-7 px-5 font-bold border-8 rounded-full border-purple-500">
+              <span>{minutes < 10 ? `0${minutes}` : minutes}</span>{" "}
+              <span className="block">Minutes</span>
+            </p>
+            <p className=" border-dashed py-7 px-4 font-bold border-8 rounded-full border-red-500">
+              <span className="">{seconds < 10 ? `0${seconds}` : seconds}</span>
+              <span className="block">Seconds</span>
+            </p>
+          </div>
         </div>
+        <div className="w-[90%] lg:w-[75%] m-auto text-xl font-montserrat tracking-wide">
+          <p className="py-5 font-semibold">
+            {" "}
+            God of all impossibilities is annaul program which was initated by
+            then then vicar <span>Ven Azubuike</span>. Initially called{" "}
+            <span>God of Hannah</span>, the program had it first outing in the
+            october 2017 and it has since then expressed a tremendous growth
+            over the years.
+          </p>
+          <div>
+            <p className="font-bold font-poppins text-emerald-800 py-5">
+              Why you shouldn't miss
+            </p>
+            <p>
+              God of all impossibilities is one program that Heaven and it host
+              has used to prove the power of the finish work of Christ in St
+              peter's Anglican Church Keffi and to the land of keffi at large.
+              Recording an increase in numbers yearly, the aimed to teach,
+              equip, develop and raise testifies for Christ not only to keffi
+              but to the nation and the world at large.
+            </p>
+          </div>
+        </div>
+        <div className="w-[90%] lg:w-[75%] m-auto text-xl font-montserrat lg:grid grid-cols-2 gap-6 tracking-wide mt-14 ">
+          {daetails.map((detail, index) => (
+            <div
+              key={index}
+              className="shadow-xl rounded-md col-span-1  bg-gray-200 mt-10 lg:mt-0 rounded-br-3xl"
+            >
+              <span className="block text-emerald-900 font-bold pt-5">
+                {detail.image}
+                {detail.title}
+              </span>
+              <p className="p-5">{detail.content}</p>
+            </div>
+          ))}
+        </div>
+        <div className="w-[90%] lg:w-[75%] m-auto text-xl font-montserrat lg:grid grid-cols-2 gap-6 tracking-wide mt-14 ">
+          <h2 className="text-emerald-900 font-bold pt-2 pb-7">SERMONS</h2>
+          <div className=" shadow-lg">
+            <video src={goai} controls className="pb-4"></video>
+            <div>
+              <p>Tittle: The move of the spirit</p>
+              <p className="py-1"> Speaker: Rev Peters</p>
+              <span className="text-blue-700 text-lg bg-orange-900 p-2 rounded-md">
+                watch full sermon
+              </span>
+            </div>
+          </div>
+        </div>
+        <Footer />
       </div>
     </div>
   );
