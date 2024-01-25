d5aa5e969926c9fd4ffcdb257e27a4d42946a908
emirael <emmzex19@gmail.com>
2024-01-25 10:14:27 +0100

    created a data file and added contents to my content component

--- src/pages/New/ContentPage.jsx
+++ src/pages/New/ContentPage.jsx
@@ -1,13 +1,37 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import { useParams } from "react-router-dom";
+import data from "./Data";
+import Footer from "../../components/footer/Footer";
 
 export default function ContentPage() {
+  const { id } = useParams();
+  const index = id - 1;
+
   return (
     <div>
       <>
-        <Header page={"TITTLE"} />
+        <Header page={data[id].title} />
       </>
-      <div></div>
+      <div className="m-auto w-[100%] lg:w-[60%] text-center text-textCol-secondary">
+        <div>
+          <img
+            src={data[index].image}
+            alt=""
+            className="w-full h-[30vh] object-cover"
+          />
+          <p className="p-5 text-xl leading-10 tracking-wide lg:tracking-widest">
+            {data[index].details}
+          </p>
+          <div className="flex justify-evenly my-10 text-green-700 py-3 tracking-wide text-xl border-2 mx-10">
+            <p className=" ">{data[index].author}</p>{" "}
+            <p>
+              {data[index].date.day} <span>{data[index].date.month}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/New/Data.js
@@ -0,0 +1,30 @@
+import back from "./assests/Church.jpg";
+const data = [
+  {
+    id: 1,
+    image: back,
+    date: { day: "01", month: "	JAN" },
+    title: "PCC NOMINATION @ ST PETE CHURCH KEFFI",
+    author: "Edwin Emmanuel",
+    details: `Ven Dr Luka Allu J.P(vicar in charge) on sunday the 21st of January, led the the nomiation of the incoming PCC members from each group. He earlier told the church late last year that each group should get ready as they will be nominating members from their respectives groups to represent them in the church planning commitee. 10, 6 and 2 memebers were nominated to represent the men women and youth respectively. This nominations were carried out succesful andthe following names were nominated to represnted the men Barrister kenchuks Barrister Barrister Barrister Barrister Barrister Barrister Barrister Barrister. The following names were nominated to represent the women Barrister Barrister Barrister Barrister Barrister Barrister. They following were nominated to represent the youth Barrister Barrister. The elections comes on febrruay 2024`,
+  },
+  {
+    id: 2,
+    image: back,
+    date: { day: "01", month: "	JAN" },
+    title: "PCC NOMINATION @ ST PETER'S ANGLICAN CHURCH KEFFI",
+  },
+  {
+    id: 3,
+    image: back,
+    date: { day: "01", month: "	JAN" },
+    title: "PCC NOMINATION @ ST PETER'S ANGLICAN CHURCH KEFFI",
+  },
+  {
+    id: 4,
+    image: back,
+    date: { day: "01", month: "	JAN" },
+    title: "PCC NOMINATION @ ST PETER'S ANGLICAN CHURCH KEFFI",
+  },
+];
+export default data;
